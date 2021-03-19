4ae8d00c46e253943a2878c7b7d050af72417d5b
MilitaerMilitz <69317314+MilitaerMilitz@users.noreply.github.com>
2021-03-19 12:39:00 +0100

    Init

---
+++ src/main/java/com/github/militaermilitz/catchtheboat/CatchTheBoat.java
@@ -0,0 +1,17 @@
+package com.github.militaermilitz.catchtheboat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CatchTheBoat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
